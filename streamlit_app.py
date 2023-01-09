835773fea02da6a74ba0a987297fabffd7393602
Phil <122314874+phenderson0613@users.noreply.github.com>
2023-01-09 15:28:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 
 streamlit.title('Title Lunch')
-streamlit.header('Header parents DIner')
-streamlit.text('Text Breakfast')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
