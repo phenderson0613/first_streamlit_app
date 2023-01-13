da6d617f7e8eca7c3cffa003342ab5c63f1c3084
Phil <122314874+phenderson0613@users.noreply.github.com>
2023-01-13 11:27:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,9 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 #New Section to display frutyvipe api
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
